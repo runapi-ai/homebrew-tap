e0fa05e1cc02d7cbb7152afbbc2f4afc8231a211
RunAPI <contact@runapi.ai>
2026-07-21 15:39:32 +0800

    Update runapi formula to v0.6.0
    Point Homebrew at the RunAPI CLI v0.6.0 release artifacts.

--- Formula/runapi.rb
+++ Formula/runapi.rb
@@ -1,26 +1,26 @@
 class Runapi < Formula
   desc "RunAPI command-line client"
   homepage "https://runapi.ai"
-  version "0.5.0"
+  version "0.6.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/runapi-ai/cli/releases/download/v0.5.0/runapi_0.5.0_Darwin_arm64.tar.gz"
-      sha256 "1bac982b887a97894323b399beb197cec219d8688e67493144ab91bea3913441"
+      url "https://github.com/runapi-ai/cli/releases/download/v0.6.0/runapi_0.6.0_Darwin_arm64.tar.gz"
+      sha256 "9355f1b33bbaeb7d6de50d71a9372591ebae64e4b29712a97f165ca892ca38d5"
     else
-      url "https://github.com/runapi-ai/cli/releases/download/v0.5.0/runapi_0.5.0_Darwin_x86_64.tar.gz"
-      sha256 "c67a82c9b59ac3f478b85cedacad5278ba1636fe9a00406d62041228ee9eb995"
+      url "https://github.com/runapi-ai/cli/releases/download/v0.6.0/runapi_0.6.0_Darwin_x86_64.tar.gz"
+      sha256 "7cdd8e52e542b1d8e5264e1526ed8a539e736265905dc134c5a0523889e9cc4b"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/runapi-ai/cli/releases/download/v0.5.0/runapi_0.5.0_Linux_arm64.tar.gz"
-      sha256 "7a67666f2e9c4396f79f672acbee58914f5a86e093e433234ddb670be661dbea"
+      url "https://github.com/runapi-ai/cli/releases/download/v0.6.0/runapi_0.6.0_Linux_arm64.tar.gz"
+      sha256 "b07413063104b002010a55efc99fddd623e33861fa8a64bd0ba8be1dd0b6d98f"
     else
-      url "https://github.com/runapi-ai/cli/releases/download/v0.5.0/runapi_0.5.0_Linux_x86_64.tar.gz"
-      sha256 "46b0a5d208297bc7354dbaefdb620fd7c0ef0d22a192e5aca37774b44973f28e"
+      url "https://github.com/runapi-ai/cli/releases/download/v0.6.0/runapi_0.6.0_Linux_x86_64.tar.gz"
+      sha256 "5e2afa60e0acc2f35d260b58fc43e6dbff6a65d5df6146e1c3d829f15e6f3d3e"
     end
   end
 
@@ -29,6 +29,6 @@ class Runapi < Formula
   end
 
   test do
-    assert_match "\"version\":\"0.5.0\"", shell_output("#{bin}/runapi version")
+    assert_match "\"version\":\"0.6.0\"", shell_output("#{bin}/runapi version")
   end
 end
